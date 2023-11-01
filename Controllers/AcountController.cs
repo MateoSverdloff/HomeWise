2cd1f7f6f5c637ca22aa1b5f1d23121431feaf5e
MATEO SVERDLOFF <47572192@ort>
2023-11-01 10:07:15 -0300

    intentando conectar con base de datos

---
+++ Controllers/AcountController.cs
@@ -0,0 +1,76 @@
+using System.Diagnostics;
+    using Microsoft.AspNetCore.Mvc;
+    using HomeWise.Models;
+
+    namespace HomeWise.Controllers;
+    
+
+
+    public class AcountController : Controller
+
+
+{
+    public IActionResult Registrarse()
+    {
+        return View("Acount");
+    }
+
+    public IActionResult IniciarSesion()
+    {
+        return View("IniciarSes");
+    }
+
+     [HttpPost]
+     public IActionResult AgregarAcount(string nombre, string username, string contraseña, string email, int telefono){
+        int cont=0;
+        Usuario user = new Usuario( nombre, username, contraseña, email, telefono);
+        BD.AgregarCuenta(user);
+        ViewBag.roni = user;
+        List<string> _usuarios=BD.ListarUsuarios();
+        foreach (string usuas in _usuarios){
+            if(usuas==username){
+                cont++;
+                Console.WriteLine(cont);
+            }
+         }
+         if(cont >=2){
+            ViewBag.Error = "El nombre de usuario ya existe";
+            return View("Acount");
+         }
+         else{ return View("MostrarDatos");}
+    }
+
+    public IActionResult Olvide()
+    {
+        return View("OlvideContraseña");
+    }
+
+    [HttpPost]
+    public IActionResult MostrarContraseña(string username)
+    {
+        ViewBag.cont=BD.OlvideCont(username);
+        if(ViewBag.cont!=null){
+            return View("MostrarContraseña");
+        }
+        else{
+            ViewBag.Error="Nombre de Usuario invalido";
+        }
+        return View("OlvideContraseña");
+    }
+
+ [HttpPost]
+     public IActionResult Login(string username, string contraseña){
+        ViewBag.roni =BD.VerificarCuenta(username, contraseña);
+        if (ViewBag.roni!=null)        
+        {
+                  return View("MostrarDatos");
+        }
+        else
+        {
+            ViewBag.Error = "Usuario o Contraseña invalidos";
+            return View("IniciarSes");
+        }
+     }
+   
+     
+}
--- Models/BD.cs
+++ Models/BD.cs
@@ -1,2 +1,49 @@
 using System.Data.SqlClient;
 using Dapper;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+
+public static class BD
+{
+    private static string _connectionString = @"server=localhost;DataBase=HomeWise;Trusted_connection=True;";
+    
+public static void AgregarCuenta(Usuario user){
+    string SQL = "INSERT INTO Usuario(Nombre,UserName,Contraseña,Email,Telefono) VALUES (@nombre, @username, @contraseña, @email, @telefono)";
+     using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            db.Execute(SQL, new { nombre = user.Nombre, username = user.UserName, contraseña = user.Contraseña, email = user.Email, telefono = user.Telefono});
+        }
+}
+
+public static List<string> ListarUsuarios(){
+    List<string> _usuarios=null;
+     using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string SQL = "SELECT UserName FROM Usuario";
+            _usuarios = db.Query<string>(SQL).ToList();;
+        }
+        return _usuarios;
+}
+public static Usuario VerificarCuenta(string username, string contraseña){
+    Usuario roni = new Usuario();
+     using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string SQL = "SELECT * FROM Usuario WHERE UserName = @pUserName AND Contraseña = @pContraseña";
+            roni = db.QueryFirstOrDefault<Usuario>(SQL, new {pUserName = username, pContraseña = contraseña });
+        }
+        return roni;
+}
+public static Usuario OlvideCont(string username){
+    Usuario roni = null;
+     using (SqlConnection db = new SqlConnection(_connectionString))
+        {
+            string SQL = "SELECT Contraseña FROM Usuario WHERE UserName = @pUserName";
+            roni = db.QueryFirstOrDefault<Usuario>(SQL, new {pUserName = username});
+        }
+        return roni;
+}
+
+
+}
